b27f8b3d88a7cc46010cfb32446be383ef49f403
Luana de Paula <51202496+luanadplr@users.noreply.github.com>
2023-06-29 11:44:38 -0300

    explicando o js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
-const interval = setInterval(()=>{
+const interval = setInterval(()=>{ //horario em tempo real
     let hora = new Date()
     let horario = hora.getHours()
     let minutos = hora.getMinutes()
-    if (minutos<=9){
+    if (minutos<=9){ //para configurar a forma que é mostrado os minutos
         let horas = document.querySelector("p#horario").innerHTML = `${horario}:0${minutos}`
     } else{
         let horas = document.querySelector("p#horario").innerHTML = `${horario}:${minutos}`
@@ -14,7 +14,7 @@ var igual = document.querySelector("button#igual")
 igual.addEventListener('click', mostrarResultado)
 
 let resume = document.querySelector("p#resumoConta")
-function adicionar(tecla){
+function adicionar(tecla){ //mostrar a tecla que foi clicada
     resume.append(tecla)
 }
 
@@ -23,13 +23,13 @@ function mostrarResultado(){
     if (equacao){
         try {
             let resultado = document.querySelector("p#resultado")
-            if (eval(equacao) == parseInt(eval(equacao))) {
+            if (eval(equacao) == parseInt(eval(equacao))) { //equação inteira
                 resultado.innerHTML = eval(equacao)
-            } if (eval(equacao) == eval(equacao).toFixed(1)){
+            } if (eval(equacao) == eval(equacao).toFixed(1)){ //equação com um número após a vírgula
                 resultado.innerHTML = eval(equacao).toPrecision(1)
-            } if(eval(equacao) == eval(equacao).toFixed(2)){
+            } if(eval(equacao) == eval(equacao).toFixed(2)){ //equação com dois números após a vírgula
                 resultado.innerHTML = eval(equacao).toPrecision(2)
-            } else {
+            } else { //equação com resultado irracional
                 resultado.innerHTML = eval(equacao).toPrecision(6)
             }
         } catch {
